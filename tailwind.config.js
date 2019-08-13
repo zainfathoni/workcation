106fff136e7193df4f200438185e4f4f739016ac
Zain Fathoni <zain.fathoni@gmail.com>
2019-08-13 23:54:01 +0800

    Hover, Focus, and Active Styles
    https://tailwindcss.com/course/hover-focus-and-active-styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   theme: {
     extend: {}
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    fontSize: ['responsive', 'hover']
+  },
   plugins: []
 }
